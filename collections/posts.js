acd894840201d579a8b68a1a5a5e089f843fed1a
pnunn <pnunn@intq.com.au>
2013-07-24 21:26:43 +1000

    Initial commit (a bit late, but there you go)

---
+++ collections/posts.js
@@ -0,0 +1,42 @@
+Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+      postWithSameLink = Posts.findOne({url: postAttributes.url});
+    
+  // ensure the user is logged in
+  if (!user)
+     throw new Meteor.Error(401, "You need to login to post new stories");
+
+  // ensure the post has a title
+  if(!postAttributes.title)
+    throw new Meteor.Error(422, "Please fill in a headline");
+
+  // check that there are no previous posts with the same link
+  if(postAttributes.url && postWithSameLink) {
+    throw new Meteor.Error(302, "This like has already been posted", postWithSameLink._id);
+  }
+
+  // pick out the whitelisted keys
+  var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+    title: postAttributes.title + (this.isSimulation ? '(client)':'(server)'),
+    userId: user._id,
+    author: user.username,
+    subbmitted: new Date().getTime()
+  });
+
+  // wait for 5 seconds
+ if(! this.isSimulation) {
+   var Future = Npm.require('fibers/future');
+   var future = new Future();
+   Meteor.setTimeout(function() {
+     future.ret();
+   }, 5 * 1000);
+   future.wait();
+ }
+  var postId = Posts.insert(post);
+  return postId;
+  }
+});
+
