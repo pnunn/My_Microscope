02910d988e8dfc8048b9e6f388203919c4ed8bf9
pnunn <pnunn@intq.com.au>
2013-07-25 15:00:13 +1000

    new package added for errors, this commit has package and non package so will break

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -9,11 +9,17 @@ Template.postSubmit.events({
     }
 
     Meteor.call('post', post, function(error,id) {
-      if(error)
-        return alert(error.reason);
-    });
+      if(error) {
+        // display the error to the user
+       throwError(error.reason);
 
-    Meteor.Router.to('postsList');
+       // if the error is that the post already existis, take us there
+       if(error.error === 302) 
+        Meteor.Router.to('postPage', error.details)
+      } else { 
+        Meteor.Router.to('postPage', id);
+      }
+    });
   }
 });
 
---
+++ packages/errors/errors_list.js
@@ -0,0 +1,14 @@
+Template.meteorErrors.helpers({
+  errors: function() {
+    return Meteor.errors.find();
+  }
+});
+
+Template.meteorError.rendered = function() {
+  var error = this.data;
+  Meteor.defer(function() {
+    Meteor.errors.update(error._id, {@set: {seen: true}});
+  });
+};
+
+
